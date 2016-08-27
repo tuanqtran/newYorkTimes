9b60d9cb5dafdf1bdb8e1cfbf9aca508af0d46d5
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-27 12:48:49 -0500

    start of assignment

---
+++ newYorkTimes.js
@@ -0,0 +1,3 @@
+// Api key: 9d4a8986921972b65754ea0809d47c84:12:74623931
+
+// http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Obama&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931
